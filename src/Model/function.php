3f9f5d931079933da9ac38b69a8d6a136d3b2b40
Younesta <boulkaddid.younes@gmail.com>
2015-05-03 19:15:12 +0200

    add function.php

---
+++ src/Model/function.php
@@ -0,0 +1,18 @@
+<?php 
+
+function addMessageFlash($type, $message)
+    {
+        $types = ['success','error','alert','info'];
+        if (!isset($types[$type])){
+            return false;
+        }
+
+        if (!isset($_SESSION['flashBag'][$type])) {
+            $_SESSION['flashBag'][$type] = [];
+        }
+
+        // on ajoute le message
+        $_SESSION['flashBag'][$type][] = $message;
+    }
+
+?>
